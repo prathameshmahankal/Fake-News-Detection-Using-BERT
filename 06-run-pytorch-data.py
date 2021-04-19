abde201af2bb6a30deb1dbfb7bcfca5583b94895
Prathamesh Mahankal <pmahankal@hirezstudios.com>
2021-04-19 08:44:36 -0700

    Adding files from local

---
+++ 06-run-pytorch-data.py
@@ -0,0 +1,43 @@
+from azureml.core import Workspace
+from azureml.core import Experiment
+from azureml.core import Environment
+from azureml.core import ScriptRunConfig
+from azureml.core import Dataset
+
+if __name__ == "__main__":
+    # ws = Workspace.from_config()
+    from azureml.core import Workspace, Experiment, Environment, ScriptRunConfig
+    from azureml.core.authentication import InteractiveLoginAuthentication
+
+    interactive_auth = InteractiveLoginAuthentication(tenant_id="72f988bf-86f1-41af-91ab-2d7cd011db47")
+    ws = Workspace.from_config(auth=interactive_auth)
+
+    datastore = ws.get_default_datastore()
+    dataset = Dataset.File.from_files(path=(datastore, 'datasets/cifar10'))
+
+    experiment = Experiment(workspace=ws, name='day1-experiment-data')
+
+    # A ScriptRunConfig packages together the configuration information needed 
+    # to submit a run in Azure ML, including the script, compute target, 
+    # environment, and any distributed job-specific configs.
+    
+    config = ScriptRunConfig(
+        source_directory='./src',
+        script='trainShort.py',
+        compute_target='cpu-cluster',
+        arguments=[
+            '--data_path', dataset.as_named_input('input').as_mount(),
+            '--learning_rate', 0.003],
+    )
+    # set up pytorch environment
+    env = Environment.from_conda_specification(
+        name='pytorch-env',
+        file_path='./.azureml/pytorch-env.yml'
+    )
+    config.run_config.environment = env
+
+    run = experiment.submit(config)
+    aml_url = run.get_portal_url()
+    print("Submitted to compute cluster. Click link below")
+    print("")
+    print(aml_url)
